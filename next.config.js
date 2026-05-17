65f8ce2695394361f4467010e0c05f79349825a5
dmooz35-rgb <dmooz35@gmail.com>
2026-05-17 07:43:53 +0300

    Add: Next.js configuration for three.js optimization and Vercel compatibility

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    config.externals.push({
+      'bufferutil': 'bufferutil',
+      'utf-8-validate': 'utf-8-validate',
+    });
+    return config;
+  },
+  experimental: {
+    optimizePackageImports: ['three'],
+  },
+};
+
+export default nextConfig;
